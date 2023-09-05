5399c589baaf9cb80bb822da9bd778d58f671267
ShahjeeStudent20 <ahmedstudent.shajee@gmail.com>
2023-09-05 20:10:04 +0500

    .

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const int appId = 1078058969;
-  static const String appSign =
-      "6cee727d8e24531fc437002c9ace6b3fb00450106157ddac95022df6059c4acd";
+  static const int appId = 92323;
+  static const String appSign = "your appsign";
 }
+ // your personal appid and appsign will be genrated from zegocloud
